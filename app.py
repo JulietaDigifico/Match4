e4ac8ad716e5b7af2bba592e096f1e05137090b9
Julieta <julieta.digifico@gmail.com>
2024-02-20 12:36:16 -0300

    Match 4

---
+++ app.py
@@ -0,0 +1,53 @@
+import tkinter
+from tkinter.messagebox import showinfo as alert
+from tkinter.messagebox import askyesno as question
+from tkinter.simpledialog import askstring as prompt
+import customtkinter
+
+
+'''
+nombre: Julieta
+apellido: Digifico
+---
+Ejercicio: Match_04
+---
+Enuciado:
+Al presionar el botón ‘Informar’ mostrar mediante alert los siguientes mensajes 
+en función del mes seleccionado:
+    Si tiene 28 días
+    Si tiene 30 días
+    Si tiene 31 días
+'''
+
+
+class App(customtkinter.CTk):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.title("UTN FRA")
+        
+        self.label_meses = customtkinter.CTkLabel(master=self, text="Meses")
+        self.label_meses.grid(row=0, column=0, padx=20, pady=10)
+        meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+        self.combobox_mes = customtkinter.CTkComboBox(master=self, values=meses)
+        self.combobox_mes.grid(row=1, column=0, padx=20, pady=(10, 10))
+        
+        self.btn_informar = customtkinter.CTkButton(master=self, text="Informar", command=self.btn_informar_on_click)
+        self.btn_informar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
+        
+    
+    def btn_informar_on_click(self):
+        mes_seleccionado = self.combobox_mes.get()
+        match mes_seleccionado:
+            case "Enero" | "Marzo" | "Mayo" | "Julio" | "Agosto" | "Octubre" | "Diciembre":
+                alert("Mensaje", "Este mes tiene 31 dias")
+            case "Febrero":
+                alert("Mensaje", "Este mes tiene 28 dias")
+            case "Abril" | "Junio" | "Septiembre" | "Noviembre":
+                alert("Mensaje", "Este mes tiene 30 dias")
+    
+if __name__ == "__main__":
+    app = App()
+    app.geometry("300x300")
+    app.mainloop()
